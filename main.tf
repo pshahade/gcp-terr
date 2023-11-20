93f62e0abbdb0abeb1995f2fd905614f3a007622
Shahade <PS06190@de.corp.devoteam.com>
2023-11-20 15:02:06 +0100

    updated terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,29 @@
-resource "google_storage_bucket" "auto_expire_paraga_test" {
-  name          = "ts_parag310923923923"
-  project       = "prj-g-s-shahade-p-522a33fe"
-  location      = "EU"
-  force_destroy = true
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
 }
