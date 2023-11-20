ab69cf907bcf24831dcdb8834548113d82058ed2
Shahade <PS06190@de.corp.devoteam.com>
2023-11-20 15:06:43 +0100

    storage bucket terraform

--- main.tf
+++ main.tf
@@ -1,29 +1,8 @@
-resource "google_compute_instance" "default" {
-  name         = "my-instance"
-  machine_type = "n2-standard-2"
-  zone         = "us-central1-a"
+resource "google_storage_bucket" "auto_expire_paraga_test" {
+  name          = "ts_parag310923923923"
+  project = "prj-g-s-shahade-p-522a33fe"
+  location      = "eu"
+  force_destroy = true
+    public_access_prevention = "enforced"
 
-  tags = ["foo", "bar"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "value"
-      }
-    }
-  }
-
-  // Local SSD disk
-  scratch_disk {
-    interface = "NVME"
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
 }
