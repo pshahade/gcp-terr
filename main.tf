78d05688d3e39fcf048353108fa22c7c47281efa
Shahade <PS06190@de.corp.devoteam.com>
2023-11-20 14:37:25 +0100

    commit updated terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "auto_expire_paraga_test" {
   name          = "auto-expiring-bucket-tst-parag"
-  location      = "eu"
-  force_destroy = false
+  location      = "EU"
+  force_destroy = true
 }
