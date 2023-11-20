90e333a0631d7f6b79dc0402db2b804b6ebc3048
Shahade <PS06190@de.corp.devoteam.com>
2023-11-20 14:13:21 +0100

    commit updated terraform

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "5.6.0"
     }
   }
@@ -9,6 +9,6 @@ terraform {
 
 provider "google" {
   # Configuration options
-  project     = "prj-g-s-shahade-p-522a33fe"
-  region      = "europe-west1"  # Change this to your desired region
+  project = "prj-g-s-shahade-p-522a33fe"
+  region  = "europe-west1" # Change this to your desired region
 }
