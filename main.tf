1483196540261576a09005cd10f56f0109bf5c9c
Shahade <PS06190@de.corp.devoteam.com>
2023-11-20 14:57:20 +0100

    updated terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "auto_expire_paraga_test" {
   name          = "ts_parag310923923923"
-  project = "prj-g-s-shahade-p"
+  project       = "prj-g-s-shahade-p-522a33fe"
   location      = "EU"
   force_destroy = true
 }
